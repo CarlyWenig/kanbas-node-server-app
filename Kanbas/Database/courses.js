2082fe7275485ee2beeb6f9ad30e7952ecb0acf3
CarlyWenig <69254490+CarlyWenig@users.noreply.github.com>
2024-03-30 19:14:32 -0400

    first commit

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    _id: "RS101",
+    name: "CS1234 Rocket Propulsion",
+    number: "CS4550.1263.202410",
+    semester: "Spring 2024 Semester Full Term",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    image: "computer.jpg",
+  },
+  {
+    _id: "RS102",
+    name: "AI3843 Aerodynamics",
+    number: "AI4560.14565.234567",
+    semester: "Spring 2024 Semester Full Term",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    image: "bio.jpg",
+  },
+  {
+    _id: "RS103",
+    name: "SD1456 Spacecraft Design",
+    number: "SD4570.14565.234567",
+    semester: "Spring 2024 Semester Full Term",
+    startDate: "2023-01-10",
+    endDate: "2023-05-15",
+    image: "flowers.jpg",
+  },
+];
